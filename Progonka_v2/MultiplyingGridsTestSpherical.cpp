ad65de8ea5a1ca18292c155da1b70abd8c69a04f
avyarkov <jarkov.alexej@gmail.com>
2022-10-25 13:15:22 +0300

    no significant code changes test #3 with r_in=0 check potential bug

--- Progonka_v2/MultiplyingGridsTestSpherical.cpp
+++ Progonka_v2/MultiplyingGridsTestSpherical.cpp
@@ -46,12 +46,22 @@ void runMultiplyingGridsTestSpherical() {
 		coeff.print(&f_out);
 		double* res_P = new double[size];	// MyDataTest results: Phi_P, Phi_M
 		double* res_M = new double[size];
-		Progonka(in, out, coeff, res_P, res_M);
+		if (dg.r[in] == 0) {
+			Progonka(in + 1, out, coeff, res_P, res_M);
+			leftSphericalSupplement(dg, gamma, coeff, res_P, res_M);
+		}
+		else {
+			coeff.el[in] = 0;
+			dg.B_in = 0;
+			coeff.kl[in] = 0;
+			Progonka(in, out, coeff, res_P, res_M);
+		}
 		f_out << "result Phi_P:" << "\n";
 		writeArray(res_P, in, out, &f_out);
 		f_out << "\n" << "result Phi_M:" << "\n";
 		writeArray(res_M, in, out, &f_out);
 		plot(dg.r, res_P, in, out, "plot" + std::to_string(j) + ".png");
+		plot(dg.r, res_P, in, in + dg.num / 5, "plotBeg" + std::to_string(j) + ".png");
 		f_out << "\n~~~~~~~\n";
 	}
 
