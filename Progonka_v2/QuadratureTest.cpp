2a08e4cb866071db741818f479b040cf1dcc5a08
avyarkov <jarkov.alexej@gmail.com>
2022-11-14 23:25:49 +0300

    Quadrature_Gauss–Legendre: orders up to 12

--- Progonka_v2/QuadratureTest.cpp
+++ Progonka_v2/QuadratureTest.cpp
@@ -1,21 +1,27 @@
+#include <iostream>
+#include <stdio.h>
 #include "Quadrature.h"
 #include "FileIO.h"
-#include <iostream>
+
+using namespace std;
 
 double f(double x) { return x * x; }
 
 void runQuadratureTest() {
 	Quadrature quadrature = Quadrature("Quadrature_Gauss–Legendre.txt");
-	int order = 5;
-	double* root = quadrature.rootsOfOrder(order);
-	double* weight = quadrature.weightsOfOrder(order);
-	for (int i = 0; i < order; i++) {
-		std::cout << root[i] << " " << weight[i] << "\n";
+	for (int i = quadrature.minOrder; i <= quadrature.maxOrder; i++) {
+		int order = i;
+		double* root = quadrature.rootsOfOrder(order);
+		double* weight = quadrature.weightsOfOrder(order);
+		printf("order = %d\n", order);
+		writeArrayEndl(root, 0, order - 1, &cout);
+		writeArrayEndl(weight, 0, order - 1, &cout);
+		double* funcVal = new double[order];
+		for (int i = 0; i < order; i++) {
+			funcVal[i] = f(root[i]);
+		}
+		double result = quadrature.integrate(order, funcVal); // expecting result = 2/3 = 0.6666667
+		printf("result = %0.15lf", result);
+		cout << "\n-----------\n";
 	}
-	std::cout << "-------\n";
-	double* funcVal = new double[order];
-	for (int i = 0; i < order; i++) {
-		funcVal[i] = f(root[i]);
-	}
-	std::cout << quadrature.integrate(order, funcVal); // expecting result = 2/3 = 0.6666667
 }
